507ced88bdf4e3fc1c8dcb4dcc4c889b62c9f6f6
BeMain <bemain.dev@gmail.com>
2022-12-12 19:12:17 +0100

    Add more colors

--- src/calendar_colors.py
+++ src/calendar_colors.py
@@ -1,10 +1,14 @@
 calendar_colors: dict[str, int | None] = {
     "#51FF51": None,  # Övrigt
     "#9393FF": 1,  # Fysik
+    "#D5D1AE": 1,  # ? Filosofi
+    "#D0FE8B": 1,  # ? Psykologi
     "#C8F759": 2,
     "#80FF80": 2,  # Idrott
     "#80FF00": 2,  # Mentorstid
-    "#DF00DF": 3,
+    "#66BDA3": 2,  # Naturkunskap
+    "#DF00DF": 3,  # Svenska
+    "#FF46FF": 3,  # Svenska som andraspråk
     "#FF8080": 4,
     "#E9C187": 4,  # ?
     "#FFFF00": 5,  # Kör
@@ -15,8 +19,9 @@ calendar_colors: dict[str, int | None] = {
     "#00FFFF": 7,  # Frukostkonsert
     "#FFFFFF": 8,  # Lunch + Gymnasiearbete
     "#0080FF": 9,  # Matte
-    "#808000": 10,  # ? Biologi
+    "#808000": 10,  # Biologi
     "#008000": 10,
+    "#002F17": 10,  # ? Religion
 }
 
 
