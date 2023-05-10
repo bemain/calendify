51069567cebfe2571f48f20a1b4398dc86710ac0
BeMain <bemain.dev@gmail.com>
2023-05-10 11:59:40 +0200

    Use python script to generate token.json file

---
+++ generate_token_file.py
@@ -0,0 +1,21 @@
+import sys
+import json
+
+credentials = json.loads(sys.argv[1])
+
+print(credentials)
+
+token = sys.argv[2]
+#token_expiry = sys.argv[3]
+
+with open('token.json', 'w') as token_file:
+    json.dump({"token": token,
+               "token_uri": "https://oauth2.googleapis.com/token",
+               "client_id": credentials["installed"]["client_id"],
+               "client_secret": credentials["installed"]["client_secret"],
+               "scopes": [
+                   "https://www.googleapis.com/auth/calendar"
+               ],
+               "refresh_token": ""
+               # "expiry": token_expiry,
+               }, token_file)
